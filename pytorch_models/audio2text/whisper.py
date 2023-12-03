218ca3e4a06a6070041f0ec92d29bbeeba61b21c
Thien Tran <gau.nernst@yahoo.com.sg>
2023-12-03 16:09:11 +0800

    simplify loading

--- pytorch_models/audio2text/whisper.py
+++ pytorch_models/audio2text/whisper.py
@@ -39,27 +39,26 @@ class WhisperEncoder(nn.Module):
         return x
 
     @staticmethod
-    def from_openai(variant: str, pretrained: bool = False) -> "WhisperEncoder":
-        n_layers, d_model, n_mels, ckpt_hash = {
-            "tiny": (4, 384, 80, "65147644a518d12f04e32d6f3b26facc3f8dd46e5390956a9424a650c0ce22b9"),
-            "tiny.en": (4, 384, 80, "d3dd57d32accea0b295c96e26691aa14d8822fac7d9d27d5dc00b4ca2826dd03"),
-            "base": (8, 512, 80, "ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e"),
-            "base.en": (8, 512, 80, "25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead"),
-            "small": (12, 768, 80, "9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794"),
-            "small.en": (12, 768, 80, "f953ad0fd29cacd07d5a9eda5624af0f6bcf2258be67c92b79389873d91e0872"),
-            "medium": (24, 1024, 80, "345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1"),
-            "medium.en": (24, 1024, 80, "d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f"),
-            "large-v1": (32, 1280, 80, "e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a"),
-            "large-v2": (32, 1280, 80, "81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524"),
-            "large-v3": (32, 1280, 128, "e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb"),
-        }[variant]
+    def from_openai(model_tag: str, pretrained: bool = False) -> "WhisperEncoder":
+        n_layers, d_model, ckpt_hash = {
+            "tiny": (4, 384, "65147644a518d12f04e32d6f3b26facc3f8dd46e5390956a9424a650c0ce22b9"),
+            "tiny.en": (4, 384, "d3dd57d32accea0b295c96e26691aa14d8822fac7d9d27d5dc00b4ca2826dd03"),
+            "base": (8, 512, "ed3a0b6b1c0edf879ad9b11b1af5a0e6ab5db9205f891f668f8b0e6c6326e34e"),
+            "base.en": (8, 512, "25a8566e1d0c1e2231d1c762132cd20e0f96a85d16145c3a00adf5d1ac670ead"),
+            "small": (12, 768, "9ecf779972d90ba49c06d968637d720dd632c55bbf19d441fb42bf17a411e794"),
+            "small.en": (12, 768, "f953ad0fd29cacd07d5a9eda5624af0f6bcf2258be67c92b79389873d91e0872"),
+            "medium": (24, 1024, "345ae4da62f9b3d59415adc60127b97c714f32e89e936602e85993674d08dcb1"),
+            "medium.en": (24, 1024, "d7440d1dc186f76616474e0ff0b3b6b879abc9d1a4926b7adfa41db2d497ab4f"),
+            "large-v1": (32, 1280, "e4b87e7e0bf463eb8e6956e646f1e277e901512310def2c24bf0e11bd3c28e9a"),
+            "large-v2": (32, 1280, "81f7c96c852ee8fc832187b0132e569d6c3065a3252ed18e56effd0b6a73e524"),
+            "large-v3": (32, 1280, "e5b1a55b89c1367dacf97e3e19bfd829a01529dbfdeefa8caeb59b3f1b81dadb"),
+        }[model_tag]
+        n_mels = 128 if model_tag == "large-v3" else 80
 
         m = WhisperEncoder(n_layers, d_model, n_mels=n_mels)
         if pretrained:
-            base_url = "https://openaipublic.azureedge.net/main/whisper/models"
-            state_dict = torch.hub.load_state_dict_from_url(
-                f"{base_url}/{ckpt_hash}/{variant}.pt", file_name=f"whisper_{variant}"
-            )
+            url = f"https://openaipublic.azureedge.net/main/whisper/models/{ckpt_hash}/{model_tag}.pt"
+            state_dict = torch.hub.load_state_dict_from_url(url, file_name=f"whisper_{model_tag}")
             state_dict = state_dict["model_state_dict"]
             state_dict = {k.replace("encoder.", ""): v for k, v in state_dict.items() if k.startswith("encoder.")}
             m.load_openai_state_dict(state_dict)
